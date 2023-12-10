5f34d0d8a39be337b0391930c8cfe78b6c85c580
Lovanirina R <lovanirina.ran@gmail.com>
2023-12-10 16:33:31 +0300

    revert serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,44 +17,15 @@ this.addEventListener("install", (event) => {
 })
 
 this.addEventListener("fetch", (event) => {
-    const isOnline = !navigator.onLine;
-    const url = new URL(event.request.url);
-
-    const isImage = url.hostname.includes(".png") || url.hostname.includes(".jpg");
-
-    if (!isOnline) {
-        if (isImage) {
-            event.respondWith(
-                caches.match(event.request).then(response => {
-                    if (response) {
-                        return response; // Return the cached image if available
-                    }
-
-                    return fetch(event.request).then(response => {
-                        if (response && response.status === 200) {
-                            // Cache the fetched image for future use
-                            event.waitUntil(
-                                caches.open('image-cache').then(cache => {
-                                    cache.put(event.request, response.clone());
-                                })
-                            );
-                        }
-                        return response;
-                    });
-                })
-            );
-        } else {
-            event.respondWith(
-                caches.match(event.request).then((resp) => {
-                    if (resp) {
-                        return resp
-                    }
-                    let requestUrl = event.request.clone();
-                    fetch(requestUrl);
-                })
-            )
-        }
-    } else {
-        event.respondWith(fetch(requestUrl));
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+                let requestUrl = event.request.clone();
+                fetch(requestUrl);
+            })
+        )
     }
 })
